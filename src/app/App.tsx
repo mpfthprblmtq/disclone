d05df525b2dde386627af3c79efce181b32f5040
mpfthprblmtq <ripley307@gmail.com>
2021-12-29 22:06:12 -0600

    initial commit

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="../../public/img/disclone512.png" className="App-logo" alt="logo" />
+        <p>Coming Soon...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
